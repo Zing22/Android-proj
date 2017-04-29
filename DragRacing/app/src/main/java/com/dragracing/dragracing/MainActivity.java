99e180a915d44d87097f65577c229a1317f6e418
Acom <282500567@qq.com>
2017-04-30 00:29:25 +0800

    增加了布局文件1.0版本，后续还需增加细节

--- DragRacing/app/src/main/java/com/dragracing/dragracing/MainActivity.java
+++ DragRacing/app/src/main/java/com/dragracing/dragracing/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dragracing.dragracing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickStartGame(View view){
+        Intent intent = new Intent(this, HouseList.class);
+        startActivity(intent);
+    }
+
+
 }
